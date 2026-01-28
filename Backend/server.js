522f45fefa23bc60edeb4d89747d25a882d84f15
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-28 09:13:42 +0530

    Update CORS origin for production deployment

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://zippydemoecom.vercel.app"
+      "https://nexen-demo-ecom.vercel.app"
     ],
     credentials: true,
   })
