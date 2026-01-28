71dde6966465775e23f953863961a9564d3eaf62
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-28 16:40:36 +0530

    Fix CORS origin URL in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://nexen-demo-ecom.vercel.app"
+      "https://nexendemoecom.vercel.app"
     ],
     credentials: true,
   })
